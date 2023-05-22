6609cb3987eb805772da04ecbbd1bd2d74aa040b
tosiek47 <p_antos@outlook.com>
2023-05-22 22:17:48 +0200

    22.05.2023

---
+++ functions.py
@@ -0,0 +1,9 @@
+def hello(first_name, last_name, age):
+    print("Hello " + first_name + " " + last_name)
+    print("Have a nice day!")
+    print("You are a "+ str(age) + " years old")
+
+my_name = "Bro"
+
+hello("Antek","Pszenica", 18)
+
---
+++ return_statement.py
@@ -0,0 +1,8 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+#print(multiply(6,8))
+
+x = multiply(6,8)
+print(x)
+
